00af7447cfb9e86c5e166ff8b9eae4c96807d19e
Stéphane De Magalhaes <stephane.de-magalhaes@epitech.eu>
2019-11-07 04:38:57 +0100

    Fix database path

--- biapp/views.py
+++ biapp/views.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class user(db.Model):
