89be9a9545b945be8103ac18601a7d0cdd36acbd
jannis <jbrugge@students.uni-mainz.de>
2019-11-14 03:24:22 +0100

    multi prozessing

--- prediction.py
+++ prediction.py
@@ -1,11 +1,10 @@
 import sys
-from typing import Any, Tuple
-
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from multiprocessing import Pool
 
-from sklearn.model_selection import train_test_split
+
 
 
 class BestList:
@@ -49,7 +48,7 @@ class BestList:
 
 
 def most_relevant_user(matrix, user_row, number_of_user, col_joke_begin,
-                     col_joke_end ):
+                       col_joke_end):
     """
     :param matrix:
     :param user_row:
@@ -57,9 +56,11 @@ def most_relevant_user(matrix, user_row, number_of_user, col_joke_begin,
     :return:
     """
     best_user = BestList(number_of_user)
-    user_row_np = np.array(user_row)[col_joke_begin : col_joke_end]
+    user_row_np = (np.array(user_row)[col_joke_begin: col_joke_end])
+    user_row_np[user_row_np == 99] = 0
     for index, row in matrix.iterrows():
-        row_np = np.array(row)[col_joke_begin : col_joke_end]
+        row_np = np.array(row)[col_joke_begin: col_joke_end]
+        row_np[row_np == 99] = 0
         corr_value = np.dot(user_row_np, row_np) / \
                      (np.linalg.norm(user_row_np) * np.linalg.norm(row_np))
         best_user.add(corr_value, row.USER_ID)
@@ -82,8 +83,8 @@ def calculate_rating(matrix, user_row, joke_index, number_most_similar_user, col
     :return:
         prediction for one field in matrix
     """
-    index_of_row_containing_joke = matrix.index[matrix.iloc[:, joke_index] == 0]
-    relevant_matrix = matrix.drop(index_of_row_containing_joke)
+    index_of_row_not_containing_joke = matrix.index[matrix.iloc[:, joke_index] == 99]
+    relevant_matrix = matrix.drop(index_of_row_not_containing_joke)
     corr_values, user_ids = most_relevant_user(relevant_matrix, user_row, number_most_similar_user,
                                                col_joke_begin, col_joke_end)
     rating_total = 0
@@ -98,7 +99,8 @@ def calculate_rating(matrix, user_row, joke_index, number_most_similar_user, col
     return rating_total / corr_value_total
 
 
-def make_prediction(df, Threshold_min_rated_jokes, number_most_similar_user, start_index_jokes, end_index):
+def make_prediction(df, Threshold_min_rated_jokes, number_most_similar_user, start_index_jokes, end_index,
+                    number_random_user_to_compare):
     """
     :param df: df has the form:
     #rated_jokes, joke_1 , ... , joke_n, UserID, Need_to_change
@@ -117,26 +119,39 @@ def make_prediction(df, Threshold_min_rated_jokes, number_most_similar_user, sta
     df_prediction matrix with all redicted values
     df_true_values_and_prediction matrix with all entries
     """
-    df_prediction = df.copy()
-    df_true_values_and_prediction = df.copy()
-    df_replaced = pd.concat([df.iloc[:, 0], df.iloc[:, 1:-2].replace(99, 0), df.iloc[:, -2:]], axis=1)
+    df_replaced = df.copy()  # .concat([df.iloc[:, 0], df.iloc[:, 1:-2].replace(99, np.NaN), df.iloc[:, -2:]], axis=1)
     index_of_row_under_threshold = df_replaced.index[df_replaced.iloc[:, 0] < Threshold_min_rated_jokes]
     df_relevant = df_replaced.drop(index_of_row_under_threshold)
     end_index_jokes = df.shape[1] + end_index
-    for i in tqdm(range(df.shape[0])):
-        user_row = df_replaced.iloc[i, :].copy()
-        if user_row['NEED_TO_CHANGE'] == 1:
-            for column in range(start_index_jokes, end_index_jokes, 1):
-                if df.iloc[i, column] == 99:
-                    rating = calculate_rating(df_relevant.ix[np.random.choice(df_relevant.index, 10)],
-                                              user_row, column, number_most_similar_user, start_index_jokes, end_index_jokes)
-                    df_prediction.iloc[i, column] = round(rating, 0)
-                    df_true_values_and_prediction.iloc[i, column] = round(rating, 0)
-                else:
-                    df_prediction.iloc[i, column] = 99
-    df_true_values_and_prediction["NEED_TO_CHANGE"] = 0
+    index_col_NEED_TO_CHANGE = df.columns.get_loc("NEED_TO_CHANGE")
+
+    df_prediction = df.apply(make_perdiction_one_row, 1, args=(df_relevant,
+                                                               start_index_jokes, end_index_jokes,
+                                                               number_most_similar_user, number_random_user_to_compare,
+                                                               index_col_NEED_TO_CHANGE))
     df_prediction["NEED_TO_CHANGE"] = 0
-    return df_prediction, df_true_values_and_prediction
+    return df_prediction
+
+
+def make_perdiction_one_row(row, df_relevant, start_index_jokes, end_index_jokes, number_most_similar_user,
+                            number_random_user_to_compare, index_col_NEED_TO_CHANGE):
+    row_unchanged = row.copy()
+    row_prediction = np.full(row.shape[0], np.nan)
+    if row['NEED_TO_CHANGE'] == 1:
+        for column in range(start_index_jokes, end_index_jokes, 1):
+            if row.iloc[column] == 99:
+                if number_random_user_to_compare > df_relevant.shape[0]:
+                    number_random_user_to_compare = df_relevant.shape[0]
+                    if number_random_user_to_compare == 0:
+                        print("Random user table is empty, probably Threshold to high!")
+                df_random = df_relevant.ix[
+                    np.random.choice(df_relevant.index, number_random_user_to_compare, replace=False)]
+                rating = calculate_rating(df_random, row_unchanged, column, number_most_similar_user,
+                                          start_index_jokes, end_index_jokes)
+                row.iloc[column] = round(rating, 0)
+                row_prediction[column] = round(rating, 0)
+
+    return row
 
 
 def load_data(filename):
@@ -144,20 +159,54 @@ def load_data(filename):
     return df
 
 
-if len(sys.argv) != 7:
-    print("Script expect 6 Parameter 'path_to_csv', 'Threshold_min_rated_jokes', 'number_most_similar_user', start_index_jokes,"
-          " end_index_jokes and if results should be print")
+def multiprozessing(input):
+    subset = input[0]
+    Threshold_min_rated_jokes = input[1]
+    num_most_similar_user = input[2]
+    start_col_jokes = input[3]
+    end_col_jokes = input[4]
+    num_random_user_to_compare = input[5]
+    df_pred = make_prediction(subset, Threshold_min_rated_jokes, num_most_similar_user,
+                                  start_col_jokes, end_col_jokes, num_random_user_to_compare)
+    print ("Subset begin with line {} from {} is prozessed".format(input[6], input[7]))
+    return df_pred
+
+# data/test_boiled.csv 2 3 1 -2 TRUE 10
+if len(sys.argv) != 10:
+    print(
+        "Script expect 9 Parameter 'path_to_csv', 'Threshold_min_rated_jokes', 'number_most_similar_user', start_index_jokes,"
+        " end_index_jokes, if results should be print, num_random_user_to_compare, size_of_sub_table, number prozesses")
 else:
     data_matrix = load_data(sys.argv[1])
     Threshold_min_rated_jokes = int(sys.argv[2])
-    number_most_similar_user = int(sys.argv[3])
+    num_most_similar_user = int(sys.argv[3])
     start_col_jokes = int(sys.argv[4])
     end_col_jokes = int(sys.argv[5])
-    df_pred, df_all_values = make_prediction(data_matrix, Threshold_min_rated_jokes, number_most_similar_user, start_col_jokes,end_col_jokes)
-    df_pred.to_csv('results/predicated_values.csv', index=None)
-    df_all_values.to_csv('results/all_values.csv', index=None)
-    if sys.argv[6]:
-        print("df_all_values: ", df_all_values, sep="\n\n", end="\n\n")
-        print("df_pred: ", df_pred, sep="\n\n",end="\n\n")
+    num_random_user_to_compare = int(sys.argv[7])
+    begin_subset = 0
 
+    input_multiprocessing = []
+    for i in range(0, data_matrix.shape[0], int(sys.argv[8])):
+        if i + int(sys.argv[8]) > data_matrix.shape[0]:
+            begin_subset = data_matrix.shape[0] - int(sys.argv[8])
+        else:
+            begin_subset = i
+        subset = data_matrix.iloc[begin_subset:begin_subset + int(sys.argv[8]), :]
+        input_multiprocessing.append([subset, Threshold_min_rated_jokes, num_most_similar_user,
+                                      start_col_jokes, end_col_jokes, num_random_user_to_compare, begin_subset, data_matrix.shape[0]])
+
+    with Pool(int(sys.argv[9])) as p:
+        result_multi_proz = p.map(multiprozessing, input_multiprocessing)
+
+    result = pd.DataFrame(columns = data_matrix.columns)
+
+    for sub_result in result_multi_proz:
+       result = pd.concat([result, sub_result])
+    result.to_csv('results/predicated_values.csv', index=None)
+    if sys.argv[6]:
+        print("df: ", result, sep="\n\n", end="\n\n")
+
+
+        # data/jesterfinal_boiled.csv 2 3 1 -2 TRUE 10 10 6
+        # data/test_boiled.csv 2 10 1 -2 FALSE 50 100 1
 
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
+
 """
 discretization of the ratings in the values -2, -1, 0 , 1, 2
 adding column USER_ID and NEED_TO_CHANGE
@@ -12,15 +13,15 @@ Runtime behaviour:
 
 def preprocessing(filename_raw, filename_boiled):
     df = pd.read_csv(filename_raw, index_col=None, header=None)
-    np.apply_along_axis(change_values, 1, df)
-    df['USER_ID'] = np.arange(len(df))
-    df['NEED_TO_CHANGE'] = 1
-    df.to_csv(filename_boiled, index=None)
+    result = df.apply(change_values, axis=1)
+    result['USER_ID'] = np.arange(len(df))
+    result['NEED_TO_CHANGE'] = 1
+    result.to_csv(filename_boiled, index=None)
 
 
 def change_values(row):
     for column in range(1, row.shape[0], 1):
-        value = row[column]
+        value = float(row[column])
         if - 10 <= value < -6:
             row[column] = -2
         if - 6 <= value < -2:
